ec753a7196e0ab721560d21729d93cac375286cb
bethjaw <jaworskibeth@gmail.com>
2017-12-19 12:01:22 -0700

    hashing password

--- db/api.js
+++ db/api.js
@@ -2,7 +2,12 @@ var knex = require('./knex')
 
 module.exports = {
   signIn: function() {
+
   },
-  signUp: function() {
+  signUp: function(body, hash) {
+    return knex('my_user').insert({
+      'agentName': body.agentName,
+      'password': hash
+    }).returning('*')
   }
 }

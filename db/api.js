a5dd3693b108b2013015ad8b8ac5485b6aaeb915
bethjaw <jaworskibeth@gmail.com>
2017-12-19 14:24:15 -0700

    hashing and signing in

--- db/api.js
+++ db/api.js
@@ -1,8 +1,8 @@
 var knex = require('./knex')
 
 module.exports = {
-  signIn: function() {
-
+  signIn: function(body) {
+    return knex('my_user').select('*').where('agentName', body.agentId).returning('*')
   },
   signUp: function(body, hash) {
     return knex('my_user').insert({
--- routes/users.js
+++ routes/users.js
@@ -4,22 +4,26 @@ var db = require('../db/api')
 var bcrypt = require('bcrypt')
 
 router.post('/signin', function(req, res, next){
-  db.signIn()
-  .then(function(agent){
-    //Use bcrypt to log in
-      if (isMatch) {
-        //Route to /Assignment
-      }
-      else {
-        res.render('index', { title: 'gClassified', message: 'Incorrect login. Contents will self destruct' })
-      }
+    db.signIn(req.body)
+    .then(function(agent){
+      //Use bcrypt to log in
+      bcrypt.compare(req.body.password, agent[0].password, function(err, isMatch) {
+        // result needs to be isMatch in our bcrypt.compare bc that's the boolean we are looking for
+        if (isMatch) {
+          //Route to /Assignment
+          console.log("IS MATCHED!");
+        }
+        else {
+          res.render('index', { title: 'gClassified', message: 'Incorrect login. Contents will self destruct' })
+        }
+    })
   })
 })
 
 router.post('/signup', function(req,res,next){
   //Use bcrypt to Sign Up
   bcrypt.hash(req.body.password, 10, function(err, hash){
-
+    // another way is to just put req.body.password = hash - wouldn't need to change in query then
     db.signUp(req.body, hash)
     .then(function(agent){
       if (agent[0].password === req.body.password) {
